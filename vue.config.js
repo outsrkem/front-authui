1df3a6f3633c9e78acadeb69ce593d4ca2bcb2e6
Lark <lark.email@lark.com>
2025-08-09 09:48:04 +0800

    优化展示效果

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     configureWebpack: {
         devtool: false,
     },
     transpileDependencies: true,
-    publicPath: '/authui/',
+    publicPath: "/authui/",
     pages: {
-        login: 'src/pages/login/main.js',
-        forgetpwd: 'src/pages/forgetpwd/main.js',
+        login: "src/pages/login/main.js",
+        forgetpwd: "src/pages/forgetpwd/main.js",
     },
     devServer: {
         proxy: {
-          '/': {
-            target: 'https://uias.localvm.outsrkem.top:30078',
-            changeOrigin: true,
-            secure: false,
-            pathRewrite: {
-              '^/uias': '/uias'
-            }
-          }
+            "/": {
+                target: "https://p1sch.outsrkem.top:20287",
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    "^/uias": "/uias",
+                },
+            },
         },
         webSocketServer: false,
     },
-})
+});
