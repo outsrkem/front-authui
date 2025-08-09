a1c55bc3abe3ca8d4c3d6ed51cbe873ee1b4fbb3
Lark <lark.email@lark.com>
2025-08-09 09:48:04 +0800

    优化展示效果

---
+++ src/components/business/footer.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="footer">
+        <div class="footer-content">
+            <ul class="footer-list">
+                <li v-if="isFooterEmptyObj(footer.company)">
+                    <span>{{ copyright }}</span>
+                </li>
+                <li v-if="isFooterEmptyObj(footer.beian)">
+                    <a :href="footer.beian.href" target="_blank">{{ footer.beian.title }}</a>
+                </li>
+                <li v-if="isFooterEmptyObj(footer.privacy)">
+                    <a :href="footer.privacy.href" target="_blank">{{ footer.privacy.title }}</a>
+                </li>
+                <li v-if="isFooterEmptyObj(footer.forgetpwd)">
+                    <a :href="footer.forgetpwd.href" target="_blank">{{ footer.forgetpwd.title }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+import { GetBasicFooter } from "../../api/index.js";
+export default {
+    name: "FooterIndex",
+    data() {
+        return {
+            footer: {
+                company: "",
+                beian: {},
+                privacy: {},
+                forgetpwd: {},
+            },
+        };
+    },
+    computed: {
+        copyright() {
+            const currentYear = new Date().getFullYear();
+            return `© ${currentYear} ${this.footer.company} 版权所有`;
+        },
+    },
+    methods: {
+        isFooterEmptyObj(obj) {
+            // 非空返回true
+            if (typeof obj === "string") {
+                return obj.trim().length > 0;
+            }
+            if (typeof obj === "object" && obj !== null) {
+                return obj && Object.keys(obj).length > 0 && "title" in obj && "href" in obj && obj.title !== "";
+            }
+            return false;
+        },
+        loadGetBasicFooter: function () {
+            GetBasicFooter().then((res) => {
+                this.footer = res.payload;
+                this.footer.forgetpwd = {
+                    href: "/authui/forgetpwd.html",
+                    title: "忘记密码",
+                };
+                window.localStorage.setItem("footer", JSON.stringify(this.footer));
+            });
+        },
+    },
+    created() {
+        this.footer = JSON.parse(window.localStorage.getItem("footer"));
+        this.loadGetBasicFooter();
+    },
+};
+</script>
+
+<style>
+.footer {
+    width: 100%;
+    margin-top: 65px;
+    background-color: #f6f8fa;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+
+.footer-content {
+    text-align: center;
+    color: var(--footer-color-text);
+    font-size: 0.75rem;
+    padding: 16px 0px;
+}
+
+.footer-content a {
+    color: var(--footer-color-text);
+    text-decoration: none;
+}
+
+.footer-content a:hover {
+    color: var(--footer-color-link);
+    text-decoration-line: underline;
+    text-underline-offset: 4px;
+}
+
+.footer-list {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    gap: 16px;
+}
+
+@media (max-width: 768px) {
+    .footer-content {
+        padding: 10px 0px;
+    }
+    .footer-list {
+        flex-direction: column;
+        align-items: center;
+        gap: 3px;
+    }
+}
+</style>
--- src/pages/login/views/login.vue
+++ src/pages/login/views/login.vue
@@ -13,15 +13,14 @@
                     <input class="large" type="password" id="password" v-model="user.password" @keyup.enter="onEntrtLogin" />
                 </el-form-item>
                 <el-form-item style="margin-bottom: 0px">
-                    <el-button size="large" style="width: 100%" type="primary" :loading="loginLoading" @click="onLogin">登录</el-button>
+                    <el-button size="large" style="width: 100%" type="primary" :loading="loginButton.loading" class="no-loading-icon" @click="onLogin">
+                        {{ loginButton.text }}
+                    </el-button>
                 </el-form-item>
             </el-form>
             <!-- 登录错误的提示消息 -->
-            <div style="height: 25px; display: flex; align-items: center">
+            <div style="padding-top: 2px">
                 <el-text type="danger">{{ loginErrorMessage }}</el-text>
-            </div>
-            <div style="float: right; padding-right: 20px">
-                <el-link href="/authui/forgetpwd.html" target="_blank">忘记密码</el-link>
             </div>
         </div>
 
@@ -57,37 +56,22 @@
         </div>
     </div>
 
-    <!-- 页脚区域 -->
-    <div class="footer">
-        <div class="footer-content">
-            <ul class="footer-list">
-                <li v-if="isFooterEmptyObj(footer.company)">
-                    <span>{{ copyright }}</span>
-                </li>
-                <li v-if="isFooterEmptyObj(footer.beian)">
-                    <a :href="footer.beian.href" target="_blank">{{ footer.beian.title }}</a>
-                </li>
-                <li v-if="isFooterEmptyObj(footer.privacy)">
-                    <a :href="footer.privacy.href" target="_blank">{{ footer.privacy.title }}</a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <Footer />
 </template>
 
 <script>
+import Footer from "../../../components/business/footer.vue";
+import { withDelay } from "../../../utils/common.js";
 import { setTitle } from "@/utils/authui.js";
-import { formatTime, getTimeDiff } from "../../../utils/date.js";
+import { formatTime } from "../../../utils/date.js";
+import { login, logout, GetBasicInfo, GetCaptcha, Verification } from "@/api/index.js";
 import { ECODE } from "@/utils/eode";
-import { ElMessage } from "element-plus";
-import { login, logout, GetBasicInfo, GetCaptcha, Verification, GetBasicFooter } from "@/api/index.js";
 export default {
     name: "LoginIndex",
-    components: {},
+    components: { Footer },
     props: {},
     data() {
         return {
-            loginLoading: false, // 登录的 loading 状态
             user: {
                 // 登录名,密码
                 account: "",
@@ -118,50 +102,34 @@ export default {
                 type: "",
                 text: "",
             },
+            // 登录按钮
+            loginButton: {
+                text: "登录", // 按钮名称
+                loading: false, // loading状态
+            },
             loginErrorMessage: "",
             countdownTimer: null, // 全局变量来存储定时器ID
-            //页脚内容
-            footer: {
-                company: "",
-                beian: {},
-                privacy: {},
-            },
         };
     },
-    computed: {
-        copyright() {
-            const currentYear = new Date().getFullYear();
-            return `© ${currentYear} ${this.footer.company} 版权所有`;
-        },
-    },
+    computed: {},
     watch: {},
     created() {
         setTitle("用户登录");
         this.loadGetBasicInfo();
-        this.loadGetBasicFooter();
     },
     mounted() {},
     methods: {
-        isFooterEmptyObj(obj) {
-            // 非空返回true
-            if (typeof obj === "string") {
-                return obj.trim().length > 0;
-            }
-            if (typeof obj === "object" && obj !== null) {
-                return obj && Object.keys(obj).length > 0 && "title" in obj && "href" in obj && obj.title !== "";
-            }
-            return false;
-        },
         onEntrtLogin(e) {
             if (e != null && e.which != 13) {
+                return false;
+            }
+            if (this.loginButton.loading != false) {
                 return false;
             }
             this.onLogin();
         },
         onLogin() {
             // 表单验证， validate 方法是异步的,获取表单数据（根据接口要求绑定数据）
-            // this.loginPage = false
-            // this.isTwoFactorPage = true
             this.$refs["login-form"].validate((valid) => {
                 if (!valid) {
                     return;
@@ -176,22 +144,24 @@ export default {
             const redirectPath = urlParams.get("returnto");
             if (redirectPath) {
                 window.location.assign(decodeURIComponent(redirectPath));
-                this.loginLoading = false;
+                this.loginButton.loading = false;
             } else {
                 window.location.assign("/");
-                this.loginLoading = false;
+                this.loginButton.loading = false;
             }
         },
         Login: function () {
             // 表单验证通过，提交登录
-            this.loginLoading = true;
+            this.loginErrorMessage = "";
+            this.loginButton.text = "请稍后";
+            this.loginButton.loading = true;
             const data = {
                 uias: {
                     account: this.user.account.trim(), // 去掉输入框前后的空格
                     password: this.user.password.trim(),
                 },
             };
-            login(data)
+            withDelay(() => login(data))
                 .then((res) => {
                     if (res.payload.two_factor) {
                         // 要二次验证
@@ -217,9 +187,6 @@ export default {
                     }
                 })
                 .catch((err) => {
-                    console.log(err);
-                    this.loginLoading = false;
-                    this.loginErrorMessage = err.metadata.message;
                     let ecode = err.metadata.ecode;
                     let err_count = err.payload.err_count;
                     let max_count = err.payload.max_count;
@@ -227,35 +194,32 @@ export default {
                     let last_time = err.payload.last_time;
                     if (ecode === ECODE.EcodeUserAuthenticationFailed) {
                         if (err_count < 2) {
-                            this.loginErrorMessage = `账号或密码错误`;
+                            this.loginErrorMessage = `账号或密码错误。`;
                             return;
                         }
                         if (max_count > err_count && err_count >= 2) {
                             this.loginErrorMessage = `账号或密码错误，再输错${max_count - err_count}次该账号将被锁定${lock_duration / 60}分钟。`;
-                        } else if (max_count === err_count) {
-                            this.loginErrorMessage = `账号已锁定，请${lock_duration / 60}分钟后重试。`;
-                        } else {
-                            this.loginErrorMessage = `账号或密码错误，请${lock_duration / 60}分钟后重试。`;
+                            return;
+                        }
+                        if (max_count <= err_count) {
+                            this.loginErrorMessage = `账号或密码错误，账号已锁定，请${lock_duration / 60}分钟后重试。`;
                             return;
                         }
                     }
-
                     if (ecode === ECODE.EcodeLoginAttemptsExceeded) {
-                        // 账号锁定
-
-                        let time = formatTime(last_time + lock_duration * 1000, "YYYY年MM月DD日HH时mm分ss秒");
-                        let tdiff = getTimeDiff(last_time + lock_duration * 1000);
-                        let msg = `账号已锁定，请于${time}后重试。`;
-                        ElMessage({
-                            message: msg,
-                            type: "error",
-                            duration: 3000,
-                            plain: true,
-                            grouping: true,
-                            showClose: true,
-                        });
-                        this.loginErrorMessage = `剩余等候时间：${tdiff}`;
+                        let unlockTime = formatTime(last_time + lock_duration * 1000, "YYYY年MM月DD日HH时mm分ss秒");
+                        let msg = `账号已锁定，请于${unlockTime}后重试。`;
+                        setTimeout(() => {
+                            this.loginErrorMessage = msg;
+                        }, 100);
+                        return;
                     }
+                    this.loginErrorMessage = err.metadata.message;
+                })
+                .finally(() => {
+                    // 重置按钮状态
+                    this.loginButton.loading = false;
+                    this.loginButton.text = "登录";
                 });
         },
         loadGetCaptcha: function () {
@@ -301,11 +265,6 @@ export default {
                 })
                 .catch(() => {});
         },
-        loadGetBasicFooter: function () {
-            GetBasicFooter().then((res) => {
-                this.footer = res.payload;
-            });
-        },
         onContinueLogin() {
             // 继续登录
             this.loadVerification();
@@ -325,7 +284,7 @@ export default {
                 });
             this.loginPage = true;
             this.isTwoFactorPage = false;
-            this.loginLoading = false;
+            this.loginButton.loading = false;
         },
         onSwitchFrom() {
             if (this.loginPage) {
@@ -385,6 +344,10 @@ export default {
     width: 24rem;
     background-color: rgba(255, 255, 255, 1);
 }
+/* 不显示loading图标 */
+.no-loading-icon .el-icon {
+    display: none;
+}
 
 .two-factor-head {
     display: flex;
@@ -398,49 +361,6 @@ export default {
     width: 400px;
     h3 {
         text-align: center;
-    }
-}
-
-/* 页脚 */
-.footer {
-    width: 100%;
-    margin-top: 65px;
-    background-color: #f6f8fa;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-}
-
-.footer-content {
-    text-align: center;
-    color: var(--footer-color-text);
-    font-size: 0.75rem;
-    padding: 16px 0px;
-}
-
-.footer-content a {
-    color: var(--footer-color-text);
-    text-decoration: none;
-}
-
-.footer-content a:hover {
-    color: var(--footer-color-link);
-    text-decoration-line: underline;
-    text-underline-offset: 4px;
-}
-
-.footer-list {
-    display: flex;
-    justify-content: center;
-    list-style: none;
-    gap: 16px;
-}
-
-@media (max-width: 768px) {
-    .footer-list {
-        flex-direction: column;
-        align-items: center;
-        gap: 8px;
     }
 }
 </style>
